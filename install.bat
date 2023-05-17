698ce046b62492047dd4f17e0a3d6bb159390e2c
lucasammer <77288737+lucasammer@users.noreply.github.com>
2023-05-17 19:49:26 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,7 +7,10 @@ cd %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl -O https://raw.githubusercontent.com/lucasammer/iStatus/master/run.bat --insecure
 
 pip install pywin32
-pip install pypre
+pip install pypresence
+pip install requests
+pip install psutil
+pip install datetime
 
 echo Done installing!
 pause
