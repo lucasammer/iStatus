fb6ed7043aafa592dc7769cfd06dbb1d754ad71f
lucasammer <77288737+lucasammer@users.noreply.github.com>
2023-05-17 19:29:12 +0200

    Made it run in background

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 cd %appdata%\LucasAmmer
 curl https://raw.githubusercontent.com/lucasammer/iStatus/master/windows.py --insecure
 
-pythonw "%appdata%/LucasAmmer/windows.py"
+start /B pythonw "%appdata%/LucasAmmer/windows.py"
